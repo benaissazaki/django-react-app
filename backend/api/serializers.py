ab792e79e08305416e82a58212be3b7790221bda
lilxak <zakben45@gmail.com>
2020-09-15 14:59:17 +0100

    Setup Django REST

---
+++ backend/api/serializers.py
@@ -0,0 +1,3 @@
+# https://www.django-rest-framework.org/api-guide/serializers/
+
+from rest_framework import Serializers
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Django REST 
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
