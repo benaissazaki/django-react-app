1ff92c997a6042af9b50c79b10940121b9c59006
lilxak <zakben45@gmail.com>
2020-09-15 20:15:03 +0100

    Setup redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import Main from './MainComponent/Main';
+import { Provider } from "react-redux";
+import { store } from './redux/store';
 
 function App() {
   return (
-    <div className="App">
-      <Main/>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Main />
+      </div>
+    </Provider>
   );
 }
 
