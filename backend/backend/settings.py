d57e07631e2fe630ac8fc46c816ffbbffe878192
lilxak <zakben45@gmail.com>
2020-09-16 18:43:50 +0100

    Hide SECRET_KEY from settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from .secret_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -18,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g)cttss+zeuzmm!qur5yy!1r4hs@8d(5#w)$+&j(^fj8(9by&u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
