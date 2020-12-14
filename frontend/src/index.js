e0ae2009b476cabd958baac158121142737184e0
lilxak <zakben45@gmail.com>
2020-12-14 17:07:10 +0100

    FE: Fix redux setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,11 +8,11 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
       <App />
-    </React.StrictMode>
-  </Provider>,
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
