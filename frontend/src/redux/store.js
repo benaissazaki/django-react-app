533529d5a4bb53d5fc083a8650b5a13f3472592d
lilxak <zakben45@gmail.com>
2020-12-15 11:05:42 +0100

    FE: Remove reactstrap and setup redux dev tools

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,8 +1,11 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import { rootReducer } from "./rootReducer";
 
 export const store = createStore(
     rootReducer,
-    applyMiddleware(thunk)
+    compose(
+        applyMiddleware(thunk),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    ),
 )
